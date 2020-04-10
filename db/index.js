08855fa5117f86de0dbf0574ee80c168f7dc54e6
redlux002 <54803584+redlux002@users.noreply.github.com>
2020-04-10 22:38:12 +0800

    First Commit

---
+++ db/index.js
@@ -0,0 +1,62 @@
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+    host : 'localhost',
+    user : 'expressjs',
+    password : 'password',
+    database : 'expressjs'
+})
+
+conn.connect( (err) => {
+    if(err){throw err;}
+    console.log('Connected to database...')
+});
+
+const db = {
+    createTable: () => {
+        const sql = "CREATE TABLE test_table (id INT AUTO_INCREMENT, firstname VARCHAR(80), lastname VARCHAR(80), PRIMARY KEY(id));"
+        conn.query(sql, (err) => {
+            if(err){throw err;}
+            console.log('Table created succesfully...')
+        })
+    },
+    getAll: () => {
+        return new Promise( (resolve, reject) => {
+            const sql = "SELECT * FROM test_table";
+            conn.query(sql, (err, result) => {
+                if(err){return reject(err);}
+                return resolve(result);
+            })
+        });
+    },
+    inserData: () => {
+        return new Promise( (resolve, reject) => {
+            const data = {firstname: "Jhon Francis", lastname: "Gabuyo"};
+            const sql = "INSERT INTO test_table SET ?";
+            conn.query(sql, data, (err) => {
+                if(err){return reject(err);}
+                return resolve("Data inserted to database...");
+            })
+        });
+    },
+    updateData: (id) => {
+        return new Promise( (resolve, reject) => {
+            const data = {id: id, firstname: "Jhon Francis", lastname: "Gabuyo"};
+            const sql = "REPLACE INTO test_table SET ?";
+            conn.query(sql, data, (err) => {
+                if(err){return reject(err);}
+                return resolve("Data updated...");
+            })
+        });
+    },
+    deleteData: (id) => {
+        return new Promise( (resolve, reject) => {            
+            const sql = "DELETE FROM test_table where id = ?";
+            conn.query(sql, id, (err) => {
+                if(err){return reject(err);}
+                return resolve("Data deleted...");
+            })
+        });
+    }
+};
+
+module.exports = db
